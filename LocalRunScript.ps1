7d2bffbf99e28650e82c241b88314d36f1d446ad
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-09-29 10:53:45 +0200

    Add local run script

---
+++ LocalRunScript.ps1
@@ -0,0 +1,49 @@
+#
+# Constants
+#
+$SONARQUBE_URL = [Environment]::GetEnvironmentVariable("SONARQUBE_URL")
+$SONARQUBE_TOKEN = [Environment]::GetEnvironmentVariable("SONARQUBE_TOKEN")
+
+$dotnetScannerParameterList = @(
+    "/key:MonorepoExample_DotnetProject",
+    "/name:""Monorepo Example: .NET Project""",
+    "/v:1.0.0",
+    "/d:sonar.host.url=$SONARQUBE_URL",
+    "/d:sonar.login=$SONARQUBE_TOKEN",
+    "/d:sonar.verbose=false"
+    )
+    
+    $cliScannerParameterList = @(
+        "-D sonar.projectKey=MonorepoExample_PythonProject",
+        "-D sonar.projectName=""Monorepo Example: Python Project""",
+        "-D sonar.host.url=$SONARQUBE_URL",
+        "-D sonar.login=$SONARQUBE_TOKEN",
+        "-D sonar.sources=./src",
+        "-D sonar.source.exclusions=./src/MonorepoDotnetProject/**/*"
+)
+
+#
+# Build and analyze the .NET project
+# .NET build + SonarScanner for .NET
+#
+
+# Prepare .NET analysis
+$dotnetScannerParameters = [string]::Join(' ', $dotnetScannerParameterList)
+$beginCmd = "dotnet sonarscanner begin $dotnetScannerParameters"
+Invoke-Expression $beginCmd
+
+# .NET build and test
+dotnet build './src/MonorepoDotnetProject/MonorepoDotnetProject.sln'
+dotnet test './src/MonorepoDotnetProject/MonorepoDotnetProject.sln' --no-build
+
+# Run .NET analysis
+dotnet sonarscanner end /d:sonar.login=$SONARQUBE_TOKEN
+
+
+#
+# Analyze the Python project
+# SonarScanner CLI
+#
+$cliScannerParameters = [string]::Join(' ', $cliScannerParameterList)
+$cliScannerCmd = "sonar-scanner $cliScannerParameters"
+Invoke-Expression $cliScannerCmd
