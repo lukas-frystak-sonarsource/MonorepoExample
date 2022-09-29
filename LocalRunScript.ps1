05f7d32c7f12fe55b9dd2d787551f7444d7e93c6
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-09-29 11:34:49 +0200

    Reset main

--- LocalRunScript.ps1
+++ LocalRunScript.ps1
@@ -1,33 +1,45 @@
+# Powershell Config
+$ErrorActionPreference = "Stop"
+
 #
 # Constants
 #
 $SONARQUBE_URL = [Environment]::GetEnvironmentVariable("SONARQUBE_URL")
 $SONARQUBE_TOKEN = [Environment]::GetEnvironmentVariable("SONARQUBE_TOKEN")
+$SONARCLOUD_URL = "https://sonarcloud.io"
+$SONARCLOUD_TOKEN = [Environment]::GetEnvironmentVariable("SONARCLOUD_TOKEN")
+
+$SONAR_URL = $SONARCLOUD_URL
+$SONAR_TOKEN = $SONARCLOUD_TOKEN
 
 # .NET Analysis parameters
 $dotnetScannerParameterList = @(
-    "/key:MonorepoExample_DotnetProject",
-    "/name:""Monorepo Example: .NET Project""",
+    "/o:lukas-frystak-sonarsource",
+    "/key:lukas-frystak-sonarsource_MonorepoExample_DotnetProject",
+    #"/key:MonorepoExample_DotnetProject",
+    #"/name:""Monorepo Example: .NET Project""",
     "/v:1.0.0",
-    "/d:sonar.host.url=$SONARQUBE_URL",
-    "/d:sonar.login=$SONARQUBE_TOKEN",
+    "/d:sonar.host.url=$SONAR_URL",
+    "/d:sonar.login=$SONAR_TOKEN",
     "/d:sonar.verbose=false"
-    #"/d:sonar.pullrequest.key=2",
+    #"/d:sonar.pullrequest.key=3",
     #"/d:sonar.pullrequest.branch=lukas/test-pr",
     #"/d:sonar.pullrequest.base=main"
 )
 
 # CLI Analysis parameters
 $cliScannerParameterList = @(
-    "-D sonar.projectKey=MonorepoExample_PythonProject",
-    "-D sonar.projectName=""Monorepo Example: Python Project""",
+    #"-D sonar.projectKey=MonorepoExample_PythonProject",
+    #"-D sonar.projectName=""Monorepo Example: Python Project""",
+    "-D sonar.organization=lukas-frystak-sonarsource",
+    "-D sonar.projectKey=lukas-frystak-sonarsource_MonorepoExample_PythonProject",
     "-D sonar.projectVersion=1.0.0"
-    "-D sonar.host.url=$SONARQUBE_URL",
-    "-D sonar.login=$SONARQUBE_TOKEN",
+    "-D sonar.host.url=$SONAR_URL",
+    "-D sonar.login=$SONAR_TOKEN",
     "-D sonar.sources=./src",
     "-D sonar.exclusions=./src/MonorepoDotnetProject/**/*",
     "-D sonar.python.version=3"
-    #"-D sonar.pullrequest.key=2",
+    #"-D sonar.pullrequest.key=3",
     #"-D sonar.pullrequest.branch=lukas/test-pr",
     #"-D sonar.pullrequest.base=main"
 )
@@ -47,7 +59,7 @@ dotnet build './src/MonorepoDotnetProject/MonorepoDotnetProject.sln'
 dotnet test './src/MonorepoDotnetProject/MonorepoDotnetProject.sln' --no-build
 
 # Run .NET analysis
-dotnet sonarscanner end /d:sonar.login=$SONARQUBE_TOKEN
+dotnet sonarscanner end /d:sonar.login=$SONAR_TOKEN
 
 #
 # Analyze the Python project
--- src/MonorepoDotnetProject/MathLibrary/BasicMath.cs
+++ src/MonorepoDotnetProject/MathLibrary/BasicMath.cs
@@ -8,11 +8,6 @@ namespace MathLibrary
     {
         public static int Add(int a, int b)
         {
-            if (false)
-            {
-                return 1;
-            }
-            
             return a + b;
         }
     }
