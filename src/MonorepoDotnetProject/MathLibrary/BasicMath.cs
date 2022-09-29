4ef1e5707665c21b843250a1a94f514f2c7941eb
lukas-frystak-sonarsource <lukas.frystak@sonarsource.com>
2022-09-29 11:09:46 +0200

    Add something to .NET project

--- src/MonorepoDotnetProject/MathLibrary/BasicMath.cs
+++ src/MonorepoDotnetProject/MathLibrary/BasicMath.cs
@@ -8,6 +8,11 @@ namespace MathLibrary
     {
         public static int Add(int a, int b)
         {
+            if (false)
+            {
+                return 1;
+            }
+            
             return a + b;
         }
     }
